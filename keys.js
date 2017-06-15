e8efbeef77da611f42b7165b66f51719e1f1d0a9
fbrahman <fbrahman@gmail.com>
2017-06-15 09:54:57 -0700

    twitter left to do

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log("this is loaded");
 
 exports.twitterKeys = {
-	consumer_key:"",
-	consumer_secret:"",
-	access_token_key:"",
-	access_token_secret:""
+	consumer_key:"LHIUbonKzF51OCMbetfeNLOsE",
+	consumer_secret:"nuT9kt5cHPiQQQsMd4VK0iBLTUcSoMJ7yaMzsIXJn0JqIfpDiB",
+	access_token_key:"875383424697081856-ZHe1o1mYfFaOjoPBmsr65APAnsSOZbJ",
+	access_token_secret:"ldHShSeLmRKECIiuHXBIWZoaqMA2OngL9WQnAblvKkiQC"
 }
--- liri.js
+++ liri.js
@@ -1,29 +1,101 @@
-const twitter = require("./keys.js");
-const spotify = require("spotify");
+const liri = (function() {
 
-let command = process.argv[2];
+    const keys = require("./keys.js");
+    const Twitter = require("twitter");
+    const Spotify = require("node-spotify-api");
+    const request = require("request");
+    const fs = require("fs");
 
-switch(command){
-	case("my-tweets"):
-		console.log("tweets");
-		break;
-	case("spotify-this-song"):
-		console.log("spotify");
-		break;
-	case("movie-this"):
-		console.log("movie");
-		break;
-	case("do-what-it-says"):
-		console.log("random");
-		break;
-}
+    let cmd = process.argv[2];
+    let query = process.argv[3];
 
-spotify.search({type:"track", query:"dancing in the moonlight"}, function(err, data){
-	if (err){
-		console.log("error occured: "+ err);
-		return;
-	}
+    let logicTree = function(cmd, query) {
+        switch (cmd) {
+            case ("my-tweets"):
+                console.log("tweets");
+                break;
+            case ("spotify-this-song"):
+                _spotify(query);
+                break;
+            case ("movie-this"):
+                _movie(query);
+                break;
+            case ("do-what-it-says"):
+                _rand(query);
+                break;
+        }
 
-	console.log(data);
-})
+    };
+
+    let spotify = new Spotify({
+        id: "35203e78173846e6978ed40ac28de587",
+        secret: "54cd0ad8674a4998a227d78cb21e01fa"
+    });
+
+    let _spotify = function(query){
+
+    	query = query?query:"The Sign";
+
+    	spotify.search({ type: 'track', query: '"'+query+'"', limit: 1 }, function(err, data) {
+    	    if (err) {
+    	        return console.log('Error occurred: ' + err);
+    	    }
+
+    	    let tracks = data.tracks.items;
+
+    	    for (var i = 0; i < tracks.length ; i++) {
+    	    	let artists = tracks[i].artists[0].name;
+    	    	let songTitle = tracks[i].name;
+    	    	let preview = tracks[i].preview_url;
+    	    	let album = tracks[i].album.name;
+
+    	    	console.log(artists, songTitle, preview, album);
+    	    }
+
+    	});
+    };
+
+    let _movie = function (query){
+    	
+    	query = query?query:"Mr. Nobody";
+
+    	request("http://www.omdbapi.com/?t="+ query +"&y=&plot=short&apikey=40e9cece", function (err, res, body){	
+    		if (err) {
+    		    return console.log('Error occurred: ' + res.statusCode);
+    		}
+
+    		let movie = JSON.parse(body);
+    		let title = movie.Title;
+    		let year = movie.Year;
+    		let imdb = movie.imdbRating;
+    		let country = movie.Country;
+    		let language = movie.Language;
+    		let plot = movie.Plot;
+    		let actors = movie.Actors
+
+    		console.log(title, year, imdb, country, language, plot, actors);
+    	})
+     }
+
+     let _rand = function (query){
+     	fs.readFile("random.txt", function(err, data){
+
+     		if(err){
+     			return console.error(err);
+     		}
+
+     		let text = data.toString();
+     		let comma = text.indexOf(',');
+     		let cmd = text.slice(0, comma);
+     		let q = text.slice(comma+1, text.length);
+
+     		logicTree(cmd,q);
+     	})
+     }
+
+     console.log(keys)
+
+    logicTree(cmd,query);
+})();
+
 
